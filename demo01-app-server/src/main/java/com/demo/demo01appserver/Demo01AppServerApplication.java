aef45716fca68169b814bdfe50f0273cee289122
Dawei95271 <dawei95271@qq.com>
2023-02-11 17:40:43 +0800

    fd

---
+++ demo01-app-server/src/main/java/com/demo/demo01appserver/Demo01AppServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demo01appserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01AppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo01AppServerApplication.class, args);
+    }
+
+}
---
+++ demo01-app-server/src/test/java/com/demo/demo01appserver/Demo01AppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demo01appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01AppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
